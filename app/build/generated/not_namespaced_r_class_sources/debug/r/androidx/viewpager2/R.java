1137d12d79e3e4ffea51f944433e5038635afa1a
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2019-12-29 18:18:09 +0100

    addPost screen

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/viewpager2/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/viewpager2/R.java
@@ -46,50 +46,50 @@ public final class R {
     public static final class dimen {
         private dimen() {}
 
-        public static final int compat_button_inset_horizontal_material = 0x7f060053;
-        public static final int compat_button_inset_vertical_material = 0x7f060054;
-        public static final int compat_button_padding_horizontal_material = 0x7f060055;
-        public static final int compat_button_padding_vertical_material = 0x7f060056;
-        public static final int compat_control_corner_material = 0x7f060057;
-        public static final int compat_notification_large_icon_max_height = 0x7f060058;
-        public static final int compat_notification_large_icon_max_width = 0x7f060059;
-        public static final int fastscroll_default_thickness = 0x7f060089;
-        public static final int fastscroll_margin = 0x7f06008a;
-        public static final int fastscroll_minimum_range = 0x7f06008b;
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f060096;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f060097;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f060098;
-        public static final int notification_action_icon_size = 0x7f0600cb;
-        public static final int notification_action_text_size = 0x7f0600cc;
-        public static final int notification_big_circle_margin = 0x7f0600cd;
-        public static final int notification_content_margin_start = 0x7f0600ce;
-        public static final int notification_large_icon_height = 0x7f0600cf;
-        public static final int notification_large_icon_width = 0x7f0600d0;
-        public static final int notification_main_column_padding_top = 0x7f0600d1;
-        public static final int notification_media_narrow_margin = 0x7f0600d2;
-        public static final int notification_right_icon_size = 0x7f0600d3;
-        public static final int notification_right_side_padding_top = 0x7f0600d4;
-        public static final int notification_small_icon_background_padding = 0x7f0600d5;
-        public static final int notification_small_icon_size_as_large = 0x7f0600d6;
-        public static final int notification_subtext_size = 0x7f0600d7;
-        public static final int notification_top_pad = 0x7f0600d8;
-        public static final int notification_top_pad_large_text = 0x7f0600d9;
+        public static final int compat_button_inset_horizontal_material = 0x7f060054;
+        public static final int compat_button_inset_vertical_material = 0x7f060055;
+        public static final int compat_button_padding_horizontal_material = 0x7f060056;
+        public static final int compat_button_padding_vertical_material = 0x7f060057;
+        public static final int compat_control_corner_material = 0x7f060058;
+        public static final int compat_notification_large_icon_max_height = 0x7f060059;
+        public static final int compat_notification_large_icon_max_width = 0x7f06005a;
+        public static final int fastscroll_default_thickness = 0x7f06008a;
+        public static final int fastscroll_margin = 0x7f06008b;
+        public static final int fastscroll_minimum_range = 0x7f06008c;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f060099;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f06009a;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f06009b;
+        public static final int notification_action_icon_size = 0x7f0600ce;
+        public static final int notification_action_text_size = 0x7f0600cf;
+        public static final int notification_big_circle_margin = 0x7f0600d0;
+        public static final int notification_content_margin_start = 0x7f0600d1;
+        public static final int notification_large_icon_height = 0x7f0600d2;
+        public static final int notification_large_icon_width = 0x7f0600d3;
+        public static final int notification_main_column_padding_top = 0x7f0600d4;
+        public static final int notification_media_narrow_margin = 0x7f0600d5;
+        public static final int notification_right_icon_size = 0x7f0600d6;
+        public static final int notification_right_side_padding_top = 0x7f0600d7;
+        public static final int notification_small_icon_background_padding = 0x7f0600d8;
+        public static final int notification_small_icon_size_as_large = 0x7f0600d9;
+        public static final int notification_subtext_size = 0x7f0600da;
+        public static final int notification_top_pad = 0x7f0600db;
+        public static final int notification_top_pad_large_text = 0x7f0600dc;
     }
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f07007a;
-        public static final int notification_bg = 0x7f07007b;
-        public static final int notification_bg_low = 0x7f07007c;
-        public static final int notification_bg_low_normal = 0x7f07007d;
-        public static final int notification_bg_low_pressed = 0x7f07007e;
-        public static final int notification_bg_normal = 0x7f07007f;
-        public static final int notification_bg_normal_pressed = 0x7f070080;
-        public static final int notification_icon_background = 0x7f070081;
-        public static final int notification_template_icon_bg = 0x7f070082;
-        public static final int notification_template_icon_low_bg = 0x7f070083;
-        public static final int notification_tile_bg = 0x7f070084;
-        public static final int notify_panel_notification_icon_bg = 0x7f070085;
+        public static final int notification_action_background = 0x7f070080;
+        public static final int notification_bg = 0x7f070081;
+        public static final int notification_bg_low = 0x7f070082;
+        public static final int notification_bg_low_normal = 0x7f070083;
+        public static final int notification_bg_low_pressed = 0x7f070084;
+        public static final int notification_bg_normal = 0x7f070085;
+        public static final int notification_bg_normal_pressed = 0x7f070086;
+        public static final int notification_icon_background = 0x7f070087;
+        public static final int notification_template_icon_bg = 0x7f070088;
+        public static final int notification_template_icon_low_bg = 0x7f070089;
+        public static final int notification_tile_bg = 0x7f07008a;
+        public static final int notify_panel_notification_icon_bg = 0x7f07008b;
     }
     public static final class id {
         private id() {}
@@ -132,36 +132,36 @@ public final class R {
         public static final int action_image = 0x7f090033;
         public static final int action_text = 0x7f090039;
         public static final int actions = 0x7f09003a;
-        public static final int async = 0x7f090040;
-        public static final int blocking = 0x7f090044;
-        public static final int chronometer = 0x7f09004f;
-        public static final int dialog_button = 0x7f090060;
-        public static final int forever = 0x7f090071;
-        public static final int icon = 0x7f090078;
-        public static final int icon_group = 0x7f090079;
-        public static final int info = 0x7f09007c;
-        public static final int italic = 0x7f09007e;
-        public static final int item_touch_helper_previous_elevation = 0x7f09007f;
-        public static final int line1 = 0x7f090083;
-        public static final int line3 = 0x7f090084;
-        public static final int normal = 0x7f090098;
-        public static final int notification_background = 0x7f090099;
-        public static final int notification_main_column = 0x7f09009a;
-        public static final int notification_main_column_container = 0x7f09009b;
-        public static final int right_icon = 0x7f0900aa;
-        public static final int right_side = 0x7f0900ab;
-        public static final int tag_accessibility_actions = 0x7f0900d8;
-        public static final int tag_accessibility_clickable_spans = 0x7f0900d9;
-        public static final int tag_accessibility_heading = 0x7f0900da;
-        public static final int tag_accessibility_pane_title = 0x7f0900db;
-        public static final int tag_screen_reader_focusable = 0x7f0900dc;
-        public static final int tag_transition_group = 0x7f0900dd;
-        public static final int tag_unhandled_key_event_manager = 0x7f0900de;
-        public static final int tag_unhandled_key_listeners = 0x7f0900df;
-        public static final int text = 0x7f0900e0;
-        public static final int text2 = 0x7f0900e1;
-        public static final int time = 0x7f0900e9;
-        public static final int title = 0x7f0900ea;
+        public static final int async = 0x7f090042;
+        public static final int blocking = 0x7f090046;
+        public static final int chronometer = 0x7f090051;
+        public static final int dialog_button = 0x7f090064;
+        public static final int forever = 0x7f090076;
+        public static final int icon = 0x7f090080;
+        public static final int icon_group = 0x7f090081;
+        public static final int info = 0x7f090088;
+        public static final int italic = 0x7f09008a;
+        public static final int item_touch_helper_previous_elevation = 0x7f09008b;
+        public static final int line1 = 0x7f09008f;
+        public static final int line3 = 0x7f090090;
+        public static final int normal = 0x7f0900a4;
+        public static final int notification_background = 0x7f0900a5;
+        public static final int notification_main_column = 0x7f0900a6;
+        public static final int notification_main_column_container = 0x7f0900a7;
+        public static final int right_icon = 0x7f0900b9;
+        public static final int right_side = 0x7f0900ba;
+        public static final int tag_accessibility_actions = 0x7f0900e8;
+        public static final int tag_accessibility_clickable_spans = 0x7f0900e9;
+        public static final int tag_accessibility_heading = 0x7f0900ea;
+        public static final int tag_accessibility_pane_title = 0x7f0900eb;
+        public static final int tag_screen_reader_focusable = 0x7f0900ec;
+        public static final int tag_transition_group = 0x7f0900ed;
+        public static final int tag_unhandled_key_event_manager = 0x7f0900ee;
+        public static final int tag_unhandled_key_listeners = 0x7f0900ef;
+        public static final int text = 0x7f0900f0;
+        public static final int text2 = 0x7f0900f1;
+        public static final int time = 0x7f0900fb;
+        public static final int title = 0x7f0900fc;
     }
     public static final class integer {
         private integer() {}
@@ -171,18 +171,18 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int custom_dialog = 0x7f0c001e;
-        public static final int notification_action = 0x7f0c0031;
-        public static final int notification_action_tombstone = 0x7f0c0032;
-        public static final int notification_template_custom_big = 0x7f0c0039;
-        public static final int notification_template_icon_group = 0x7f0c003a;
-        public static final int notification_template_part_chronometer = 0x7f0c003e;
-        public static final int notification_template_part_time = 0x7f0c003f;
+        public static final int custom_dialog = 0x7f0c001f;
+        public static final int notification_action = 0x7f0c0033;
+        public static final int notification_action_tombstone = 0x7f0c0034;
+        public static final int notification_template_custom_big = 0x7f0c003b;
+        public static final int notification_template_icon_group = 0x7f0c003c;
+        public static final int notification_template_part_chronometer = 0x7f0c0040;
+        public static final int notification_template_part_time = 0x7f0c0041;
     }
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0f002c;
+        public static final int status_bar_notification_info_overflow = 0x7f0f0030;
     }
     public static final class style {
         private style() {}
--- app/src/main/java/com/example/blog_app/addPost/AddPostFragment.kt
+++ app/src/main/java/com/example/blog_app/addPost/AddPostFragment.kt
@@ -1,15 +1,29 @@
 package com.example.blog_app.addPost
 
+import android.R.*
+import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 import com.example.blog_app.R
+import kotlinx.android.synthetic.main.add_post_fragment.*
+import android.widget.ArrayAdapter as ArrayAdapter
 
 class AddPostFragment : Fragment() {
+
+    lateinit var option: Spinner
+
+    lateinit var edit: EditText
 
     companion object {
         fun newInstance() = AddPostFragment()
@@ -21,7 +35,69 @@ class AddPostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.add_post_fragment, container, false)
+        val view = inflater.inflate(R.layout.add_post_fragment, container, false)
+        val spinner = view.findViewById(R.id.spinner) as Spinner
+        val edit = view.findViewById<EditText>(R.id.post)
+        val error1 =view.findViewById<TextView>(R.id.error1)
+
+        option = spinner
+
+        val categories = arrayListOf("Choose Category", "Sport", "News", "Entertainment", "Fashion", "Travel")
+        option.adapter = ArrayAdapter<String>(
+            requireActivity(),
+            layout.simple_list_item_1,
+            categories
+        )
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+                if(categories.get(position)!="Choose Category"){
+                    error1.visibility= View.GONE
+                    option.setBackgroundResource(R.drawable.edittext)
+                }
+                addPost.setOnClickListener{ view ->
+
+                    if(categories.get(position)=="Choose Category"){
+                        error1.visibility= View.VISIBLE
+//                        option.setBackgroundResource(R.drawable.edittexterror)
+                    }else{
+
+                        error1.visibility= View.GONE
+                    }
+//
+                    if(edit.text.isEmpty()){
+                        error1.visibility= View.VISIBLE
+//                        edit.setBackgroundResource(R.drawable.edittexterror)
+                    }else{
+                        error1.visibility = View.GONE
+                    }
+                }
+
+                edit.addTextChangedListener(object : TextWatcher {
+                    override fun afterTextChanged(s : Editable?){}
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    }
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                        error1.visibility = View.GONE
+                        edit.setBackgroundResource(R.drawable.edittext)
+
+                    }
+                })
+            }
+        }
+
+        return view
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
