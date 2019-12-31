2dd3730686b550002f7f6462429dfb8ee421743e
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2019-12-31 10:54:14 +0100

    addpostviewmodel

--- app/src/main/java/com/example/blog_app/addPost/AddPostViewModel.kt
+++ app/src/main/java/com/example/blog_app/addPost/AddPostViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.blog_app.addPost
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.blog_app.model.Post
+import com.example.blog_app.model.PostRepository
 
-class AddPostViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AddPostViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val postRepository =PostRepository.getRepository(application)
+
+//    private val posts: MutableLiveData<List<Post>> by lazy {
+//        MutableLiveData<List<Post>>().also {
+//            loadPosts()
+//        }
+//    }
+
+    fun savePost(post: Post){
+        postRepository.savePost(post)
+    }
 }
